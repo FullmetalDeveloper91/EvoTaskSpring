0be01615feff4a242d109968129f4243857df141
Andrey Chudotvorov <lonelywolf796@gmail.com>
2025-03-12 23:57:51 +0700

    Project EvoSpring is complete

--- src/main/java/ru/fmd/EvoSpring/controller/PersonController.java
+++ src/main/java/ru/fmd/EvoSpring/controller/PersonController.java
@@ -1,96 +1,65 @@
 package ru.fmd.EvoSpring.controller;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.fmd.EvoSpring.dto.Message;
 import ru.fmd.EvoSpring.dto.Person;
-import ru.fmd.EvoSpring.repository.PersonRepository;
 import ru.fmd.EvoSpring.services.PersonService;
-
-import java.util.NoSuchElementException;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/person")
 public class PersonController {
 
     private final PersonService service;
-    private final PersonRepository repository;
 
-    public PersonController(PersonService service, PersonRepository repository) {
+    public PersonController(PersonService service) {
         this.service = service;
-        this.repository = repository;
     }
 
     @GetMapping
     public Iterable<Person> getPerson() {
-        return repository.findAll();
+        return service.findAll();
     }
 
     @GetMapping("/{p_id}")
-    public Optional<Person> findPersonById(@PathVariable("p_id") int id) {
-        return repository.findById(id);
+    public ResponseEntity<Person> findPersonById(@PathVariable("p_id") int personId) {
+        return service.findById(personId);
     }
 
     @PostMapping
     public ResponseEntity<Person> addPerson(@RequestBody Person person) {
-        repository.save(person);
-        return new ResponseEntity<>(person, HttpStatus.CREATED);
+        return service.addPerson(person);
     }
 
     @PutMapping("/{p_id}")
     public ResponseEntity<Person> updatePerson(@PathVariable("p_id") int id, @RequestBody Person person) {
-        var oldPerson = repository.findById(id).orElse(null);
-
-        if(oldPerson != null){
-            oldPerson.setFirstname(person.getFirstname());
-            oldPerson.setSurname(person.getSurname());
-            oldPerson.setLastname(person.getLastname());
-            oldPerson.setBirthday(person.getBirthday());
-
-            return new ResponseEntity<>(repository.save(oldPerson), HttpStatus.OK);
-        }else
-            return addPerson(person);
+        return service.updatePerson(id, person);
     }
 
     @DeleteMapping("/{p_id}")
-    public void deletePerson(@PathVariable("p_id") int id) {
-        repository.deleteById(id);
+    public void deletePerson(@PathVariable("p_id") int personId) {
+        service.delete(personId);
     }
 
     @GetMapping("/{p_id}/message")
-    public Iterable<Message> getPersonMessage(@PathVariable("p_id") int personId){
-        return repository.findById(personId).get().getMessages();
+    public ResponseEntity<Iterable<Message>> getPersonMessages(@PathVariable("p_id") int personId){
+        return service.getPersonMessages(personId);
+    }
+
+    @GetMapping("/{p_id}/message/{m_id}")
+    public ResponseEntity<Message> getPersonMessage(
+            @PathVariable("p_id") int personId,
+            @PathVariable("m_id") int messageId){
+        return service.getPersonMessage(personId, messageId);
     }
 
     @PostMapping("/{p_id}/message")
-    public ResponseEntity<Person> addMessage(@PathVariable("p_id") int id, @RequestBody Message message){
-        Person person = null;
-        HttpStatus status;
-
-        try{
-            person = service.addMessageToPerson(id, message);
-            status = HttpStatus.OK;
-        }catch (NoSuchElementException ex){
-            status = HttpStatus.BAD_REQUEST;
-        }
-
-        return new ResponseEntity<>(person,status);
+    public ResponseEntity<Person> addMessage(@PathVariable("p_id") int personId, @RequestBody Message message){
+        return service.addMessageToPerson(personId, message);
     }
 
     @DeleteMapping("{p_id}/message/{m_id}")
-    public HttpStatus deleteMessage(@PathVariable("p_id") int personId, @PathVariable("m_id") int messageId){
-        HttpStatus status;
-
-        try{
-            if(service.delMessageByID(personId, messageId))
-                status = HttpStatus.OK;
-            else
-                status = HttpStatus.BAD_REQUEST;
-        }catch (NoSuchElementException ex){
-            status = HttpStatus.BAD_REQUEST;
-        }
-        return status;
+    public void deleteMessage(@PathVariable("p_id") int personId, @PathVariable("m_id") int messageId){
+       service.delMessageByID(personId, messageId);
     }
 }
--- src/main/java/ru/fmd/EvoSpring/services/PersonService.java
+++ src/main/java/ru/fmd/EvoSpring/services/PersonService.java
@@ -1,12 +1,16 @@
 package ru.fmd.EvoSpring.services;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 import ru.fmd.EvoSpring.dto.Message;
 import ru.fmd.EvoSpring.dto.Person;
 import ru.fmd.EvoSpring.repository.PersonRepository;
 
 import java.time.LocalDateTime;
-import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @Service
 public class PersonService {
@@ -16,18 +20,91 @@ public class PersonService {
         this.repository = repository;
     }
 
-    public Person addMessageToPerson(int personId, Message message) throws NoSuchElementException {
-        Person person = repository.findById(personId).orElseThrow();
+    public Iterable<Person> findAll() {
+        return repository.findAll();
+    }
+
+    public ResponseEntity<Person> findById(int personId){
+        Person person = repository.findById(personId).orElse(null);
+
+        return person==null
+                ? new ResponseEntity<>(null, HttpStatus.BAD_REQUEST)
+                : new ResponseEntity<>(person, HttpStatus.OK);
+    }
+
+    public ResponseEntity<Person> addPerson(Person person) {
+        repository.save(person);
+        return new ResponseEntity<>(person, HttpStatus.CREATED);
+    }
+
+    public ResponseEntity<Person> updatePerson(int id, Person person) {
+        var oldPerson = repository.findById(id).orElse(null);
+
+        if(oldPerson != null){
+            oldPerson.setFirstname(person.getFirstname());
+            oldPerson.setSurname(person.getSurname());
+            oldPerson.setLastname(person.getLastname());
+            oldPerson.setBirthday(person.getBirthday());
+
+            return new ResponseEntity<>(repository.save(oldPerson), HttpStatus.OK);
+        }else
+            return addPerson(person);
+    }
+
+    @Transactional
+    public void delete(int personId) {
+        if(repository.findById(personId).isEmpty())
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+
+        repository.deleteById(personId);
+    }
+
+    public ResponseEntity<Iterable<Message>> getPersonMessages(int personId) {
+        Person person = repository.findById(personId).orElse(null);
+
+        return person == null
+                ? new ResponseEntity<>(null, HttpStatus.BAD_REQUEST)
+                : new ResponseEntity<>(person.getMessages(), HttpStatus.OK);
+    }
+
+    public ResponseEntity<Message> getPersonMessage(int personId, int messageId) {
+        Optional<Person> person = repository.findById(personId);
+
+        if(person.isEmpty())
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+        else{
+            Message message = person.get().getMessages().stream()
+                    .filter(m -> m.getId() == messageId)
+                    .findFirst().orElse(null);
+            return message==null
+                    ? new ResponseEntity<>(null, HttpStatus.BAD_REQUEST)
+                    : new ResponseEntity<>(message, HttpStatus.OK);
+        }
+    }
+
+    @Transactional
+    public ResponseEntity<Person> addMessageToPerson(int personId, Message message) {
+        Person person = repository.findById(personId).orElse(null);
+
+        if(person == null)
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+
         message.setPerson(person);
         message.setTime(LocalDateTime.now());
         person.addMessage(message);
-        return repository.save(person);
+
+        return new ResponseEntity<>(person, HttpStatus.OK);
     }
 
-    public boolean delMessageByID(int personId, int messageId){
-        Person person = repository.findById(personId).orElseThrow();
-        boolean result = person.removeMessage(messageId);
-        repository.save(person);
-        return result;
+    @Transactional
+    public void delMessageByID(int personId, int messageId){
+        Person person = repository.findById(personId).orElse(null);
+
+        if(person == null){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }else {
+            if(!person.removeMessage(messageId))
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
     }
 }
